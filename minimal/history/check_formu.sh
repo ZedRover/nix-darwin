3c72185c0fafe0e79030533619955f2a4258fbfb
yu.wang <yu.wang@outlook.my>
2023-08-02 09:28:11 +0800

    Scripted auto-commit on change (2023-08-02 09:28:10) by gitwatch.sh

--- minimal/history/check_formu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while read p; do
-  if grep -q "$p" nix_packages.txt; then
-    echo $p >> can_install.txt
-  else
-    echo "\"$p\"" >> cannot_install.txt
-  fi
-done <brew_formula_list.txt
