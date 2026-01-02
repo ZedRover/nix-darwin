07f8d4af6bb17d91065338b6906c670deba15d33
yu.wang <yu.wang@outlook.my>
2026-01-02 09:56:16 +0800

    feat: Enable Homebrew mirror and add SunloginControl

--- minimal/modules/apps.nix
+++ minimal/modules/apps.nix
@@ -50,11 +50,11 @@
     ### END Nix ###
   ];
   environment.variables = {
-    # HOMEBREW_API_DOMAIN = "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api";
-    # HOMEBREW_BOTTLE_DOMAIN = "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles";
-    # HOMEBREW_BREW_GIT_REMOTE = "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git";
-    # HOMEBREW_CORE_GIT_REMOTE = "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git";
-    # HOMEBREW_PIP_INDEX_URL = "https://pypi.tuna.tsinghua.edu.cn/simple";
+    HOMEBREW_API_DOMAIN = "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api";
+    HOMEBREW_BOTTLE_DOMAIN = "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles";
+    HOMEBREW_BREW_GIT_REMOTE = "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git";
+    HOMEBREW_CORE_GIT_REMOTE = "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git";
+    HOMEBREW_PIP_INDEX_URL = "https://pypi.tuna.tsinghua.edu.cn/simple";
   };
   homebrew = {
     enable = true;
@@ -158,6 +158,7 @@
       "rustdesk"
       "skim"
       "snipaste"
+      "sunlogincontrol"
       "telegram"
       #"temurin@21"
       "tencent-lemon"
