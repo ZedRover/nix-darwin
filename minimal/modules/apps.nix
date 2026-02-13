947646e4f8a100ac5fbe0f98edaab7ed4859c0e9
yu.wang <yu.wang@outlook.my>
2026-02-13 17:24:47 +0800

    feat: Comment out Homebrew mirrors and update proxy ports

--- minimal/modules/apps.nix
+++ minimal/modules/apps.nix
@@ -50,14 +50,14 @@
     ### END Nix ###
   ];
   environment.variables = {
-    HOMEBREW_API_DOMAIN = "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api";
-    HOMEBREW_BOTTLE_DOMAIN = "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles";
-    HOMEBREW_BREW_GIT_REMOTE = "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git";
-    HOMEBREW_CORE_GIT_REMOTE = "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git";
-    HOMEBREW_PIP_INDEX_URL = "https://pypi.tuna.tsinghua.edu.cn/simple";
-    http_proxy = "http://127.0.0.1:6152";
-    https_proxy = "http://127.0.0.1:6152";
-    all_proxy = "socks5://127.0.0.1:6153";
+    # HOMEBREW_API_DOMAIN = "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles/api";
+    # HOMEBREW_BOTTLE_DOMAIN = "https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles";
+    # HOMEBREW_BREW_GIT_REMOTE = "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/brew.git";
+    # HOMEBREW_CORE_GIT_REMOTE = "https://mirrors.tuna.tsinghua.edu.cn/git/homebrew/homebrew-core.git";
+    # HOMEBREW_PIP_INDEX_URL = "https://pypi.tuna.tsinghua.edu.cn/simple";
+    http_proxy = "http://127.0.0.1:7890";
+    https_proxy = "http://127.0.0.1:7890";
+    all_proxy = "socks5://127.0.0.1:7891";
   };
   homebrew = {
     enable = true;
@@ -131,6 +131,7 @@
       "chatgpt-atlas"
       "claude"
       "claude-code"
+      "codex-app"
       "cursor"
       "droid"
       "easydict"
@@ -165,12 +166,12 @@
       "skim"
       "snipaste"
       "telegram"
-      "awesun"
       #"temurin@21"
       "tencent-lemon"
       "tencent-meeting"
       "termius"
       "trunk-io"
+      "uuremote"
       "visual-studio-code"
       "vnc-viewer"
       "wechat"
